c05e2285d2c3e7463655f0975ae12ab5cd15bd6b
Francisco <francisco.andraade16@gmail.com>
2026-02-08 15:09:21 -0300

    test: cria utilitario de renderizacao e implementa 5 testes para CardCesta

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from '../store';
+
+// Essa função envolve qualquer componente com o Provider do Redux e o Router
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        {children}
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+// Re-exportamos tudo do testing-library
+export * from '@testing-library/react';
+
+// Exportamos nosso render customizado
+export { customRender as render };
